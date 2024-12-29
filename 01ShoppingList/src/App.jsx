c7f0c6799181117701c2c41cf2e1b47975e20d69
Ambareeshvs <ambareeshvs@gmail.com>
2024-12-29 12:47:57 +0530

    added state lifting : data needs to transfer which has no parent child relation. Removed ddependency od data and make it dynamic

--- 01ShoppingList/src/App.jsx
+++ 01ShoppingList/src/App.jsx
@@ -1,13 +1,20 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import { useState } from "react";
 
-function App() {
+function App() 
+{
+  const [items, setItemToList] = useState([]);
+
+  const handleAddShoppingItem = (newShopList) => {
+      setItemToList((items) => [...items, newShopList])
+  };
 
   return (
     <>
       <Header />
-      <Main />
+      <Main items = {items} handleAddShoppingItem = {handleAddShoppingItem}/>
       <Footer />
     </>
   )
--- 01ShoppingList/src/components/Main.jsx
+++ 01ShoppingList/src/components/Main.jsx
@@ -1,9 +1,8 @@
 import List from "./List";
-import shoppingList from "../data/data";
 import Form from "./Form";
+import PropTypes from 'prop-types';
 
-
-function Main()
+function Main({items, handleAddShoppingItem})
 {
     return(
         <div className="flex flex-col min-h-screen">
@@ -11,12 +10,12 @@ function Main()
                 <div className="container mx-auto p-4">
                     <div className="overflow-x-auto">
                         <div className="min-w-full">
-                            <Form />
+                            <Form handleAddShoppingItem = {handleAddShoppingItem}/>
                         </div>
                         <table className="min-w-full bg-black border border-black-300 mt-2">
                             <thead className="bg-gray-200">
                                 <tr>
-                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider border-b">#</th>
+                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider border-b">#ID</th>
                                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider border-b">Product Name</th>
                                 <th className="px-6 py-3 text-center text-xs font-medium text-gray-700 uppercase tracking-wider border-b">Product Quantity</th>
                                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider border-b">Action</th>
@@ -24,7 +23,7 @@ function Main()
                             </thead>
                             <tbody>
                                 {
-                                    shoppingList.map((item) => (<List item = {item} key = {item.id}/>))
+                                    items.map((items) => (<List item = {items} key = {items.id}/>))
                                 }
                             </tbody>
                         </table>
@@ -36,5 +35,20 @@ function Main()
         
     )
 }
+
+Main.propTypes = {
+    items : PropTypes.shape({
+        id : PropTypes.number.isRequired,
+        product : PropTypes.string.isRequired,
+        quantity : PropTypes.number.isRequired,
+        packed : PropTypes.bool.isRequired
+    }).isRequired,
+    handleAddShoppingItem : PropTypes.shape({
+        id : PropTypes.number.isRequired,
+        product : PropTypes.string.isRequired,
+        quantity : PropTypes.number.isRequired,
+        packed : PropTypes.bool.isRequired
+    }).isRequired
+};
 
 export default Main;
